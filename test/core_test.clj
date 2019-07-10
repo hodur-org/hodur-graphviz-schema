a37e1ee2582bf44616dc214866d702e4a83e622f
Tiago Luchini <tiago@luchini.nyc>
2019-07-10 16:41:01 -0400

    Move README to markdown, fix links and add tests

---
+++ test/core_test.clj
@@ -0,0 +1,30 @@
+(ns core-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [hodur-graphviz-schema.core :as graphviz]
+            [hodur-engine.core :as engine]))
+
+(def basic-schema '[^{:graphviz/tag-recursive true}
+                    Person
+                    [^String first-name
+                     ^String last-name]])
+
+(def color-schema '[^{:graphviz/tag-recursive true
+                      :graphviz/color "navajowhite1"}
+                    Person
+                    [^String name]
+
+                    ^{:graphviz/tag-recursive true
+                      :graphviz/color "lightsalmon"}
+                    Business
+                    [^String total-revenue]])
+
+(deftest test-basic
+  (let [s-target (-> "basic.dot" io/resource slurp)
+        s-mine (-> basic-schema engine/init-schema graphviz/schema)]
+    (is (= s-target s-mine))))
+
+(deftest test-color
+  (let [s-target (-> "color.dot" io/resource slurp)
+        s-mine (-> color-schema engine/init-schema graphviz/schema)]
+    (is (= s-target s-mine))))
